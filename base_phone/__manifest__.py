f4d98eaf6b882ad68ccd30d8c3282b2d74b71bf1
fanch <fl@openfire.fr>
2025-01-27 12:27:12 +0100

    [IMP] base_phone: black, isort, prettier

--- base_phone/__manifest__.py
+++ base_phone/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Base Phone",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "category": "Phone",
     "license": "AGPL-3",
     "summary": "Validate phone numbers",
